251e9031073aa9ebe28d2b4a8224c26e08efed18
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-03 13:35:45 +0500

    project

---
+++ DarkWeb_BackEnd/admin-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron>
+        <h1>
+          Admin Dashboard
+        </h1>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
