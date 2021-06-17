1e34ce2712bb09db44edf9bfb20464fed71dfbb9
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-17 17:05:59 +0500

    project

--- DarkWeb_BackEnd/src/routes/auth.js
+++ DarkWeb_BackEnd/src/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require(`express`);
 const router = express.Router();
 const {signup, signin, requireSignin } = require('../controller/auth')
+const { validateRequest, isRequestValidated } = require('../validators/auth');
 
-router.post(`/signup`, signup);
+router.post(`/signup`, validateRequest, isRequestValidated, signup);
 router.post(`/signin`, signin);
 
 router.post(`/profile`, requireSignin, (req, res) => {
