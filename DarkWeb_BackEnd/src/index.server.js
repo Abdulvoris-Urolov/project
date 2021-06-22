832fc15af42675efec27a7f432bacf31ddbc0703
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-22 09:33:14 +0500

    project

--- DarkWeb_BackEnd/src/index.server.js
+++ DarkWeb_BackEnd/src/index.server.js
@@ -22,12 +22,15 @@ mongoose.connect(
     },
 ).then(() => {
     console.log(`Database connected`);
+})
+.catch((e) => {
+    console.log(e)
 });
 
 app.use(express.json());
 app.use(`/api`, authRoutes);
 app.use(`/api`, adminRoutes);
-app.use(`/api`, categoryRoutes);
+app.use(`/api/category`, categoryRoutes);
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server is running on port ${process.env.PORT}`);
