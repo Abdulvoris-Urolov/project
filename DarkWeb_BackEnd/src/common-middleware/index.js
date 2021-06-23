7ae262abab03d973b1bc83ebd1b0f975326c72cb
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-23 13:24:28 +0500

    project change

--- DarkWeb_BackEnd/src/common-middleware/index.js
+++ DarkWeb_BackEnd/src/common-middleware/index.js
@@ -1,24 +1,29 @@
-exports.requireSignin = (req, res, next) =>{
+const jwt = require('jsonwebtoken');
+
+exports.requireSignin = (req, res, next) => {
 
     if(req.headers.authorization){
-        const token = req.headers.authorization.slipt(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         const user = jwt.verify(token, process.env.JWT_SECRET);
         req.user = user;
-        next();
-    }
+
+    }else{
     return res.status(400).json({ message: 'Authorization required' });
-
-
+    }
+    next();
     //jwt.decode()
 }
 
 exports.userMiddleware = (req, res, next) => {
-
+    if(req.user.role == 'user'){
+        return res.status(400).json({ message: 'User access denied'})
+    }
+    next();
 }
 
 exports.adminMiddleware = (req, res, next) => {
-    if(req.user.role !== 'admin'){
-        return res.status(400).json({ message: 'Access denied'})
+    if(req.user.role == 'admin'){
+        return res.status(400).json({ message: 'Admin access denied'})
     }
     next();
 }
