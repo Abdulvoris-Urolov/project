b6b3068bdb58952bbbbeddd9a49196b11eefad3d
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-25 19:26:41 +0500

    project

--- DarkWeb_BackEnd/src/controller/auth.js
+++ DarkWeb_BackEnd/src/controller/auth.js
@@ -3,7 +3,7 @@ const jwt = require(`jsonwebtoken`);
 
 exports.signup = (req, res) => {
     User.findOne({ email: req.body.email })
-    .exec((error, user) => {
+    .exec((_error, user) => {
         if(user) return res.status(400).json({
             message: `User alredy regidtered`
         });
