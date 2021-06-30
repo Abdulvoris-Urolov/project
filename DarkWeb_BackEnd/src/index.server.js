962fe2428f39cde78d8db5e83a03754df4f2bd39
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-06-30 10:28:41 +0500

    add

--- DarkWeb_BackEnd/src/index.server.js
+++ DarkWeb_BackEnd/src/index.server.js
@@ -7,6 +7,7 @@ const mongoose = require(`mongoose`);
 const authRoutes = require(`./routes/auth`);
 const adminRoutes = require(`./routes/admin/auth`);
 const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
 
 //environment variable or you say constants
 env.config();
@@ -31,6 +32,7 @@ app.use(express.json());
 app.use(`/api`, authRoutes);
 app.use(`/api`, adminRoutes);
 app.use(`/api`, categoryRoutes);
+app.use(`/api`, productRoutes);
 
 app.listen(process.env.PORT, () =>{
     console.log(`Server is running on port ${process.env.PORT}`);
